ef137b676742948185b77a737768a5ca09a2a4a8
medo910 <mahmoud.0420a@gmail.com>
2025-07-28 04:08:11 +0200

    create custom appbar

---
+++ lib/widgets/custom_search_icon.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CustomSearchIcon extends StatelessWidget {
+  const CustomSearchIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 45,
+      width: 45,
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.2),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Center(
+        child: Icon(
+          Icons.search,
+          // color: Colors.black,
+          size: 30,
+        ),
+      ),
+    );
+  }
+}
